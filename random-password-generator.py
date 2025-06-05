c06778bf8cd7314f5733f1347e234035625333be
Scott <bscott04.official@gmail.com>
2025-06-05 22:15:37 +0800

    Create random-password-generator.py

---
+++ random-password-generator.py
@@ -0,0 +1,62 @@
+import random
+
+#random password generator
+#must contain at least 1 Upper Case
+#must contain at least 1 lower case
+#must contain at least 1 special character (e.g. "-", "_", "!", etc.)
+#must contain at least 1 number
+#must have a length size of at least 8 characters
+
+
+def lst_lower_cases():
+    lower_cases = []
+    for number in range(97, 122 + 1): #lowercase a-z in unicode
+        lower_cases += chr(number)
+    return lower_cases
+
+def lst_upper_cases():
+    upper_cases = []
+    for i in range(65,90+1): #uppercase A-Z in unicode
+        upper_cases+=chr(i)
+    return upper_cases
+
+def special_characters():
+    special_chr = []
+    for symbol in range(33,48+1): #special characters such as symbols
+        special_chr+=chr(symbol)
+    for symbol in range(58,64+1): #another set of characters/symbols
+        special_chr+= chr(symbol)
+    return special_chr
+
+def nums():
+    numbers = []
+    for num in range(48,57+1):   #numbers 0-9 in unicode, although 0,10 range would have sufficed
+        numbers += chr(num)
+    return numbers
+
+
+def gen_password():
+    lower = lst_lower_cases()
+    upper = lst_upper_cases()
+    spec = special_characters()
+    numbers = nums()
+    password = ""
+    
+    random_cnt = random.randint(8,17) #generates a password length of anywhere between 8 characters to 16 characters
+    
+    while len(password) <random_cnt:
+        password += random.choice(lower) + random.choice(upper) + random.choice(spec) + random.choice(numbers)
+        
+        if len(password)>=random_cnt:
+            break
+            
+    password_as_lst = list(password)
+    shuffled = random.shuffle(password_as_lst)
+    password_shuffled = ""
+    for character in password_as_lst:
+        password_shuffled += character
+        
+    return password_shuffled
+        
+        
+gen_password()
